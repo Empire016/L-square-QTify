5bc014149f88f0b2a5587ec9e232c0740c0dcb94
Rohit <rohit016more@gmail.com>
2024-03-23 18:38:07 +0530

    NavBar is Added

---
+++ qtify/src/components/Logo/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import BrandLogo from "../../assets/LogoImage.png"
+const Logo = () => {
+  return (
+    <div>
+        <img src={BrandLogo} alt='logo' width={67} />
+        </div>
+  )
+}
+
+export default Logo
